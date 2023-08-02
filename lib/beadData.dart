35aac235ed3ffa75a7ead4ffe030b0caa33c0238
Nattaphonh Phitsavath <Muramasa32@hotmail.com>
2023-08-02 17:59:43 +0700

    beadData

--- lib/beadData.dart
+++ lib/beadData.dart
@@ -1,57 +1,57 @@
 import 'dart:convert';
 
 class Beaddata {
-  final int product_id;
-  final String product_name;
-  final double product_price;
-  final int product_qty;
-  final String product_category;
+  final int productId;
+  final String productName;
+  final double productPrice;
+  final int productQty;
+  final String productCategory;
   final String image;
   Beaddata({
-    required this.product_id,
-    required this.product_name,
-    required this.product_price,
-    required this.product_qty,
-    required this.product_category,
+    required this.productId,
+    required this.productName,
+    required this.productPrice,
+    required this.productQty,
+    required this.productCategory,
     required this.image,
   });
 
   Beaddata copyWith({
-    int? product_id,
-    String? product_name,
-    double? product_price,
-    int? product_qty,
-    String? product_category,
+    int? productId,
+    String? productName,
+    double? productPrice,
+    int? productQty,
+    String? productCategory,
     String? image,
   }) {
     return Beaddata(
-      product_id: product_id ?? this.product_id,
-      product_name: product_name ?? this.product_name,
-      product_price: product_price ?? this.product_price,
-      product_qty: product_qty ?? this.product_qty,
-      product_category: product_category ?? this.product_category,
+      productId: productId ?? this.productId,
+      productName: productName ?? this.productName,
+      productPrice: productPrice ?? this.productPrice,
+      productQty: productQty ?? this.productQty,
+      productCategory: productCategory ?? this.productCategory,
       image: image ?? this.image,
     );
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'product_id': product_id,
-      'product_name': product_name,
-      'product_price': product_price,
-      'product_qty': product_qty,
-      'product_category': product_category,
+      'productId': productId,
+      'productName': productName,
+      'productPrice': productPrice,
+      'productQty': productQty,
+      'productCategory': productCategory,
       'image': image,
     };
   }
 
   factory Beaddata.fromMap(Map<String, dynamic> map) {
     return Beaddata(
-      product_id: map['product_id'].toInt() as int,
-      product_name: map['product_name'] as String,
-      product_price: map['product_price'].toDouble() as double,
-      product_qty: map['product_qty'].toInt() as int,
-      product_category: map['product_category'] as String,
+      productId: map['productId'].toInt() as int,
+      productName: map['productName'] as String,
+      productPrice: map['productPrice'].toDouble() as double,
+      productQty: map['productQty'].toInt() as int,
+      productCategory: map['productCategory'] as String,
       image: map['image'] as String,
     );
   }
@@ -62,7 +62,7 @@ class Beaddata {
 
   @override
   String toString() {
-    return 'Beaddata(product_id: $product_id, product_name: $product_name, product_price: $product_price, product_qty: $product_qty, product_category: $product_category, image: $image)';
+    return 'Beaddata(productId: $productId, productName: $productName, productPrice: $productPrice, productQty: $productQty, productCategory: $productCategory, image: $image)';
   }
 
   @override
@@ -70,21 +70,21 @@ class Beaddata {
     if (identical(this, other)) return true;
   
     return 
-      other.product_id == product_id &&
-      other.product_name == product_name &&
-      other.product_price == product_price &&
-      other.product_qty == product_qty &&
-      other.product_category == product_category &&
+      other.productId == productId &&
+      other.productName == productName &&
+      other.productPrice == productPrice &&
+      other.productQty == productQty &&
+      other.productCategory == productCategory &&
       other.image == image;
   }
 
   @override
   int get hashCode {
-    return product_id.hashCode ^
-      product_name.hashCode ^
-      product_price.hashCode ^
-      product_qty.hashCode ^
-      product_category.hashCode ^
+    return productId.hashCode ^
+      productName.hashCode ^
+      productPrice.hashCode ^
+      productQty.hashCode ^
+      productCategory.hashCode ^
       image.hashCode;
   }
 }
